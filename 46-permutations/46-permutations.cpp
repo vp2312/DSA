942b225d87dcb6729bec7ba13b94460143e335ad
vp2312 <87029058+vp2312@users.noreply.github.com>
2022-02-15 22:11:31 +0530

    Time: 35 ms (5.64%), Space: 9.3 MB (6.04%) - LeetHub

---
+++ 46-permutations/46-permutations.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+          // Code here there
+     vector<vector<int>> ans;
+     // string ip = S;
+     vector<int> op ;
+     solve(nums,op,ans);
+     sort(ans.begin(), ans.end());
+     return ans;
+    }
+private:
+
+   void solve(vector<int> ip, vector<int> op, vector<vector<int>>& ans){
+       // cout<<"ip is->"<<ip<<" "<<"op is->"<<op<<endl;
+        int n = ip.size();
+        if(n == 0){
+            ans.push_back(op);
+            cout<<"......"<<endl;
+            return;
+        }
+        for(int i = 0;i<n;i++){
+            vector<int> op1(op);
+            vector<int> ip1(ip);
+            op1.push_back(ip[i]);
+            ip1.erase(ip1.begin()+i);
+            solve(ip1,op1,ans);
+        }
+        return;
+        
+    }
+};
