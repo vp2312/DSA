5b8862eb2f74c0e96f59840f7d0d50fe13dd018d
vp2312 <87029058+vp2312@users.noreply.github.com>
2022-02-15 18:42:32 +0530

    Added solution - LeetHub

---
+++ Subsets - GFG/subsets.cpp
@@ -0,0 +1,81 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    vector<vector<int> > subsets(vector<int>& a)
+    {
+        vector<vector<int>> v;
+        int ptr=0;
+        vector<int> x;
+        solve(a,x,ptr,v);
+        sort(v.begin(),v.end());
+        return v;
+        // void solve(a,ptr,v,vec);
+        //code here
+    }
+private:
+    void solve(vector<int>& ip, vector<int>& op, int cnt, vector<vector<int>>& ans)
+    {
+        if(cnt==ip.size())
+        {
+            ans.push_back(op);
+            return ;
+        }
+        
+        vector<int> m(op);
+        vector<int> n(op);
+        n.push_back(ip[cnt]);
+        solve(ip,m,cnt+1,ans);
+        solve(ip,n,cnt+1,ans);
+        return;
+        
+        // vector<vector<int> v;
+        // int ptr=0;
+        // void solve(a,ptr,v,vec);
+        //code here
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		int n, x;
+		cin >> n;
+
+		vector<int> array;
+		for (int i = 0; i < n; i++)
+		{
+			cin >> x;
+			array.push_back(x);
+		}
+        
+        
+        Solution ob;
+		vector<vector<int> > res = ob.subsets(array);
+
+		// Print result
+		for (int i = 0; i < res.size(); i++) {
+			for (int j = 0; j < res[i].size(); j++)
+				cout << res[i][j] << " ";
+			cout << endl;
+		}
+
+		
+	}
+
+	return 0;
+}  // } Driver Code Ends
