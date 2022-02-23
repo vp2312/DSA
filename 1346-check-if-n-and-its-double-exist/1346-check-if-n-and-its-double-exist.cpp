7d931380cea1795b83c65ae312e7167510094ec1
Vivek Pathak <87029058+vp2312@users.noreply.github.com>
2022-02-23 21:32:59 +0530

    Time: 7 ms (80.13%), Space: 10 MB (82.94%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        int flag=0;
+        for(auto j:arr)
+        {
+            if(j==0 && flag==0)
+            {
+                flag=1;
+                continue;
+            }
+            if(j<0)
+            {
+                j=j*-1;
+                if(j%2==0)
+            {
+                    
+                if(find(arr.begin(),arr.end(),-j)!=arr.end() && (find(arr.begin(),arr.end(),-j/2)!=arr.end() || find(arr.begin(),arr.end(),-j*2)!=arr.end()))
+                {
+                    // cout<<*j;
+                    return true;
+                }
+            }
+            else
+            {
+                if(find(arr.begin(),arr.end(),-j)!=arr.end() && (find(arr.begin(),arr.end(),-j*2)!=arr.end()))
+                {
+                    // /cout<<*j;
+                    return true;
+                }
+                
+            }
+            }
+            else
+            {
+                if(j%2==0)
+            {
+                if(find(arr.begin(),arr.end(),j)!=arr.end() && (find(arr.begin(),arr.end(),j/2)!=arr.end() || find(arr.begin(),arr.end(),j*2)!=arr.end()))
+                {
+                    // cout<<*j;
+                    return true;
+                }
+            }
+            else
+            {
+                if(find(arr.begin(),arr.end(),j)!=arr.end() && (find(arr.begin(),arr.end(),j*2)!=arr.end()))
+                {
+                    // cout<<*j;
+                    return true;
+                }
+                
+            }
+                
+            }
+            
+        }
+       
+        return false;
+    }
+};
