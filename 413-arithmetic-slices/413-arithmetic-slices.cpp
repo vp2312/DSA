c568e61b96f008a448aa6e01fb4a0f135c82f2af
Vivek Pathak <87029058+vp2312@users.noreply.github.com>
2022-03-03 18:05:26 +0530

    Time: 4 ms (49.49%), Space: 7.4 MB (16.43%) - LeetHub

---
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        int n = nums.size();
+
+         int start, diff, len, ans;
+         start = diff = len = ans = 0;
+
+         for (int i = 1; i < n; i++) {
+            int currD = nums[i] - nums[i - 1];
+
+            if (diff != currD) {
+               len = i - start;
+               ans += (len * (len - 3)) / 2 + 1;
+               start = i - 1;
+               diff = currD;
+            }
+
+            //last index
+            if (i == n - 1) {
+               len = n - start;
+               ans += (len * (len - 3)) / 2 + 1;
+            }
+         }
+
+         return ans;
+        
+    }
+};
