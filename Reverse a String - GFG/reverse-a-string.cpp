ac10417cb4ac297e89b9e4dd224149cddf3521de
Vivek Pathak <87029058+vp2312@users.noreply.github.com>
2022-03-10 22:39:34 +0530

    Added solution - LeetHub

--- Reverse a String - GFG/reverse-a-string.cpp
+++ Reverse a String - GFG/reverse-a-string.cpp
@@ -30,7 +30,14 @@ int main() {
 //User function Template for C++
 
 string reverseWord(string str){
-    reverse(str.begin(),str.end());
+    // reverse(str.begin(),str.end());
+    
+    for(int i=0;i<str.length()/2;i++)
+    {
+        char temp= str[i];
+        str[i]=str[str.length()-1-i];
+        str[str.length()-1-i]=temp;
+    }
     return str;
   //Your code here
 }
