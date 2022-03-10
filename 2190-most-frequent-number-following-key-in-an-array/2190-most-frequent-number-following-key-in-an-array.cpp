0853193fd76a50b5a0888828b9862e289186b0a0
Vivek Pathak <87029058+vp2312@users.noreply.github.com>
2022-03-10 22:21:53 +0530

    Time: 24 ms (17.73%), Space: 12.9 MB (9.55%) - LeetHub

---
+++ 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int mostFrequent(vector<int>& nums, int key) {
+        map<int, int> mp;
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            if(nums[i]==key)
+            {
+                if(mp.find(nums[i+1])==mp.end())
+                {
+                    mp[nums[i+1]]=1;
+                }
+                else
+                {
+                    mp[nums[i+1]]++;
+                }
+            }
+        }
+        for(auto j: mp)
+        {
+            cout<<j.first<<"->"<<j.second<<endl;
+            // if(j.second > ans)
+            // {
+            //     ans=j.first;
+            //     // cout<< j.second<<" ...";
+            // }
+        }
+        int ans=INT_MIN;
+        int ans2;
+        for(auto j: mp)
+        {
+            if(j.second > ans)
+            {
+                ans=j.second;
+                ans2=j.first;
+                cout<< j.second<<" ...";
+            }
+        }
+        
+        return ans2;
+    }
+};
