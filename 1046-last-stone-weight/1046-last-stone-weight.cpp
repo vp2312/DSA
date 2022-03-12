03f82cfcf6ad0edc39a5ebc3b8618ce430bb5418
Vivek Pathak <87029058+vp2312@users.noreply.github.com>
2022-03-12 22:52:58 +0530

    Time: 3 ms (55.65%), Space: 7.7 MB (35.70%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,31 +1,39 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
+        priority_queue <int> pq(stones.begin(),stones.end());
         while (1)
         {
-            if(stones.size() ==1)
+            if(pq.size() ==1)
             {
-                return stones[0];
+                return pq.top();
             }
-            if(stones.size() == 0)
+            if(pq.size() == 0)
             {
                 return 0;
             }
             
-            sort(stones.rbegin(),stones.rend());
-            if(stones[0]==stones[1])
+            int a=pq.top();
+            pq.pop();
+            int b=pq.top();
+            pq.pop();
+            // priority_queue <int> pq;
+            
+            // sort(stones.rbegin(),stones.rend());
+            if(a!=b)
             {
-                stones.erase(stones.begin());
-                stones.erase(stones.begin());
+                pq.push(abs(a-b));
+                // stones.erase(stones.begin());
+                // stones.erase(stones.begin());
             }
-            else
-            {
-                int ans=abs(stones[0]-stones[1]);
-                cout<<ans<<"..";
-                stones.erase(stones.begin());
-                stones.erase(stones.begin());
-                stones.push_back(ans);
-            }
+            // else
+            // {
+            //     int ans=abs(stones[0]-stones[1]);
+            //     cout<<ans<<"..";
+            //     stones.erase(stones.begin());
+            //     stones.erase(stones.begin());
+            //     stones.push_back(ans);
+            // }
         }
         
         return 0;
