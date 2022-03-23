a864364b47b14234b72c34779010e5dc27514dee
Vivek Pathak <87029058+vp2312@users.noreply.github.com>
2022-03-23 18:22:11 +0530

    Added solution - LeetHub

---
+++ Kth smallest element - GFG/kth-smallest-element.cpp
@@ -0,0 +1,77 @@
+// { Driver Code Starts
+//Initial function template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{
+    public:
+    // arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
+    int kthSmallest(int arr[], int l, int r, int k) {
+        int ans;
+        if(k<(l-r)/2)
+        {
+            priority_queue<int,vector<int>,greater<int>> pq; //min heap declared
+            for(int i=l;i<=r;i++)
+            {
+                pq.push(arr[i]);       //min heap created
+            }
+            
+            // int ans;
+            while(k--)
+            {
+                ans=pq.top();
+                pq.pop();
+            }
+        }
+        else
+        {
+            priority_queue<int> pq;
+            for(int i=l;i<=r;i++)
+            {
+                pq.push(arr[i]);       //max heap created
+            }
+            int val=(r-l-k+2);
+            // int ans;
+            while(val--)
+            {
+                ans=pq.top();
+                pq.pop();
+            }
+            
+        }
+        
+        
+        return ans;
+        //code here
+    }
+};
+
+// { Driver Code Starts.
+ 
+int main()
+{
+    int test_case;
+    cin>>test_case;
+    while(test_case--)
+    {
+        int number_of_elements;
+        cin>>number_of_elements;
+        int a[number_of_elements];
+        
+        for(int i=0;i<number_of_elements;i++)
+            cin>>a[i];
+            
+        int k;
+        cin>>k;
+        Solution ob;
+        cout<<ob.kthSmallest(a, 0, number_of_elements-1, k)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
