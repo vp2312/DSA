bf444a1e1f3b8b930da6a1c9e5c47bf1f4ed3afb
Vivek Pathak <87029058+vp2312@users.noreply.github.com>
2022-03-25 18:09:12 +0530

    Time: 9 ms (35.80%), Space: 8.2 MB (27.58%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -43,12 +43,6 @@ public:
             }
         }
         cout<< a<<"  "<<b<<endl;
-        
-        // for(auto j: costs)
-        // {
-        //     ans+=min(j[0],j[1]);
-        // }
-        
         if(a==b)
         {
             return ans;
@@ -57,115 +51,26 @@ public:
         {
             if(a>b)
             {
-                
                 priority_queue<int,vector<int>,greater<int>> pq(numsa.begin(),numsa.end());
                 int val=abs(b-a)/2;
                 while(val--)
                 {
-
                     ans+=pq.top();
                     pq.pop();
-
                 }
             }
             else
             {
-                
                 priority_queue<int,vector<int>,greater<int>> pq(numsb.begin(),numsb.end());
                 int val=abs(b-a)/2;
                 while(val--)
                 {
-
                     ans+=pq.top();
                     pq.pop();
-
                 }
-            }
-                // vector<int> nums;
-            //     for(auto j: costs)
-            //     {
-            //         nums.push_back(abs(j[0]-j[1]));
-            //     }
-            // for(auto j: nums)
-            //     {
-            //         cout<<j<<" ";
-            //     }
-            // cout<<endl;
-                
+            }   
             return ans;
         }
-        
         return 1;
-        
-        
-        
-        
-        
-//         int a=0;
-//         int b=0;
-//         for(auto j: costs)
-//         {
-//             if(j[0]>j[1])
-//             {
-//                 b++;
-//             }
-//             else if(j[0]<j[1])
-//             {
-//                 a++;
-//             }
-//         }
-        
-//         if(a+b<costs.size())
-//         {
-//             if(a>b)
-//             {
-//                 b+=costs.size()-(a+b);
-//             }
-//             else if(b>a)
-//             {
-//                 a+=costs.size()-(a+b);
-//             }
-//             else
-//             {
-//                 a+=(costs.size()-(a+b))/2;
-//                 b+=(costs.size()-(a+b))/2;
-//             }
-//         }
-//         cout<< a<<"  "<<b<<endl;
-//         int ans=0;
-//         for(auto j: costs)
-//         {
-//             ans+=min(j[0],j[1]);
-//         }
-        
-//         if(a==b)
-//         {
-//             return ans;
-//         }
-//         else
-//         {
-//                 vector<int> nums;
-//                 for(auto j: costs)
-//                 {
-//                     nums.push_back(abs(j[0]-j[1]));
-//                 }
-//             for(auto j: nums)
-//                 {
-//                     cout<<j<<" ";
-//                 }
-//             cout<<endl;
-//                 priority_queue<int,vector<int>,greater<int>> pq(nums.begin(),nums.end());
-//             int val=abs(b-a)/2;
-//             while(val--)
-//             {
-                
-//                 ans+=pq.top();
-//                 pq.pop();
-                
-//             }
-//             return ans;
-//         }
-        
-//         return 1;
     }
 };
