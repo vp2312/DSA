d8071e8bc0d80cfb9d0043dc437f34562440e5fa
Vivek Pathak <87029058+vp2312@users.noreply.github.com>
2022-04-07 23:53:31 +0530

    Time: 13 ms (6.91%), Space: 9.3 MB (95.71%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,42 +1,86 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& mat, int target) {
-        int m=mat.size();
-        int n= mat[0].size();
-        // int high=
-        // while()
-        for(auto j: mat)
-        {
-            if(j[0]<=target && j[n-1]>=target)
-            {
+        
+//         int n = matrix.size();
+//         int m = matrix[0].size();
+//         int start = 0, end = m*n - 1;
+        
+//         while(start <= end)
+//         {
+//             int mid = start + (end - start) / 2;
+//             int ind = matrix[mid/m][mid%m];
+//             if (target == ind)
+//                 return true;
+//             else if(target < ind)
+//                 end = mid - 1;
+//             else
+//                 start = mid + 1;       
+//         }
+                int m=mat.size();
+                int n= mat[0].size();
                 int low= 0;
-                int high= n-1;
-                int mid= (low+high)/2;
+                int high= m*n-1;
                 while(low<=high)
                 {
-                    if(j[mid]==target)
+
+                    int mid=low + (high - low) / 2;
+                    int val=mat[mid/n][mid%n];
+                    if(val==target)
                     {
                         return true;
                     }
-                    else if(j[mid]>target)
+                    else if(val>target)
                     {
                         high = mid-1;
                     }
-                    else
+                    else if(val<target)
                     {
                         low =mid+1;
                     }
-                    
-                    mid=(low+high)/2;
                 }
                 return false;
-            }
-        }
+                
+            
+
+            // return false;
         
-        return false;
+//         int m=mat.size();
+//         int n= mat[0].size();
+//         // int high=
+//         // while()
+//         for(auto j: mat)
+//         {
+//             if(j[0]<=target && j[n-1]>=target)       // NLogN Approach
+//             {
+//                 int low= 0;
+//                 int high= n-1;
+//                 int mid= (low+high)/2;
+//                 while(low<=high)
+//                 {
+//                     if(j[mid]==target)
+//                     {
+//                         return true;
+//                     }
+//                     else if(j[mid]>target)
+//                     {
+//                         high = mid-1;
+//                     }
+//                     else
+//                     {
+//                         low =mid+1;
+//                     }
+                    
+//                     mid=(low+high)/2;
+//                 }
+//                 return false;
+//             }
+//         }
+        
+//         return false;
         
         
-//         for(int i=0;i<matrix.size();i++)
+//         for(int i=0;i<matrix.size();i++)   // N^2 Approach
 //         {
 //             for(int j=0;j<matrix[i].size();j++)
 //             {
