5e7662d81c070873ac95013248eb8ae058d1af05
Vivek Pathak <87029058+vp2312@users.noreply.github.com>
2022-04-07 19:34:48 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (70.39%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -8,18 +8,12 @@ public:
 
         while (mid <= hi) { 
             switch (a[mid]) { 
-
-            // If the element is 0 
             case 0: 
                 swap(a[lo++], a[mid++]); 
                 break; 
-
-            // If the element is 1 . 
             case 1: 
                 mid++; 
                 break; 
-
-            // If the element is 2 
             case 2: 
                 swap(a[mid], a[hi--]); 
                 break; 
