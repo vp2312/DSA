559cd36390d1615d0245eb34c2b7b34fc133cd69
Vivek Pathak <87029058+vp2312@users.noreply.github.com>
2022-04-09 19:13:30 +0530

    Added solution - LeetHub

--- Spirally traversing a matrix - GFG/spirally-traversing-a-matrix.cpp
+++ Spirally traversing a matrix - GFG/spirally-traversing-a-matrix.cpp
@@ -9,56 +9,38 @@ class Solution
     //Function to return a list of integers denoting spiral traversal of matrix.
     vector<int> spirallyTraverse(vector<vector<int> > arr, int r, int c) 
     {
-        int top=0, bot= 0, cor= r-1, right=c-1;
-        vector<int> ans;
-        while(bot<=right && cor>=top )
-        {
-            for(int i=bot;i<=right;i++)
-            {
-                // cout<<arr[top][i]<<" ";
-                ans.push_back(arr[top][i]);
-            }
-            top++;
-            // cout<<endl;
-            for(int i=top;i<=cor;i++)
-            {
-                // cout<<arr[i][right]<<" ";
-                ans.push_back(arr[i][right]);
-            }
-            right--;
-            // cout<<endl;
-            if(top<=cor)
-            {
-                for(int i=right;i>=bot;i--)
-                {
-                    // cout<<arr[cor][i]<<" ";
-                    ans.push_back(arr[cor][i]);
-                }
-                cor--;
-                
-            }
-            
-            // cout<<endl;
-            if(bot<=right)
-            {
-                for(int i=cor;i>=top;i--)
-                {
-                    // cout<<arr[i][bot]<<" ";
-                    ans.push_back(arr[i][bot]);
-                }
-                bot++;
-                
-            }
-            
-            // cout<<endl;
-            
-            // top+=1;
-            // right-=1;
-            // cor-=1;
-            // bot+=1; 
-        }
-        
-        return ans;
+        int top = 0, bot = 0, cor = r - 1, right = c - 1;
+		vector<int> ans;
+		while (bot <= right && cor >= top)
+		{
+			for (int i = bot; i <= right; i++)
+			{
+				ans.push_back(arr[top][i]);
+			}
+			top++;
+			for (int i = top; i <= cor; i++)
+			{
+				ans.push_back(arr[i][right]);
+			}
+			right--;
+			if (top <= cor)
+			{
+				for (int i = right; i >= bot; i--)
+				{
+					ans.push_back(arr[cor][i]);
+				}
+				cor--;
+			}
+			if (bot <= right)
+			{
+				for (int i = cor; i >= top; i--)
+				{
+					ans.push_back(arr[i][bot]);
+				}
+				bot++;
+			}
+		}
+		return ans;
         
         
         // int top=0, bot= 0, cor= r-1, right=c-1;
