14916a05c0a0e0445017dd5f6665b2554c6a2d41
Vivek Pathak <87029058+vp2312@users.noreply.github.com>
2022-04-09 11:02:26 +0530

    Added solution - LeetHub

--- Next Greater Element - GFG/next-greater-element.cpp
+++ Next Greater Element - GFG/next-greater-element.cpp
@@ -9,34 +9,54 @@ class Solution
     public:
     //Function to find the next greater element for each element of the array.
     vector<long long> nextLargerElement(vector<long long> arr, int n){
-        vector<long long> ans;
-        stack<long long> s;
-        s.push(arr[n-1]);
-        ans.push_back(-1);
-        int i=n-2;
-            while(i>=0)       //!s.empty() || arr[i]<s.top()
-            {
-                if(arr[i]<s.top())
-                {
-                    ans.push_back(s.top());
-                    s.push(arr[i]);
-                    i--;
-                    
-                }
-                else if(arr[i]>s.top())
-                {
-                    s.pop();
-                }
+        vector<long long>v;
+        stack<long long>s;
+        
+        for(int i= n-1;i>=0;i--)
+        {
+            if(s.size() == 0) v.push_back(-1);
+            else if(s.size() > 0 && s.top() > arr[i] )
+                    v.push_back(s.top());
+            else if(s.size()>0 && s.top() <= arr[i]){
                 
-                if(s.empty())
-                {
-                    ans.push_back(-1);
-                    s.push(arr[i]);
-                    i--;
-                }
+                while(s.size()>0 && s.top() <= arr[i])
+                      s.pop();
+                
+                if(s.size() == 0) v.push_back(-1);
+                else v.push_back(s.top());
             }
-           reverse(ans.begin(),ans.end());
-           return ans;
+            s.push(arr[i]);
+        }
+        reverse(v.begin(),v.end());
+        return v;
+        // vector<long long> ans;
+        // stack<long long> s;
+        // s.push(arr[n-1]);
+        // ans.push_back(-1);
+        // int i=n-2;
+        //     while(i>=0)       //!s.empty() || arr[i]<s.top()
+        //     {
+        //         if(arr[i]<s.top())
+        //         {
+        //             ans.push_back(s.top());
+        //             s.push(arr[i]);
+        //             i--;
+                    
+        //         }
+        //         else if(arr[i]>s.top())
+        //         {
+        //             s.pop();
+        //         }
+                
+        //         if(s.empty())
+        //         {
+        //             ans.push_back(-1);
+        //             s.push(arr[i]);
+        //             i--;
+        //         }
+        //     }
+        //   reverse(ans.begin(),ans.end());
+        //   return ans;
             
             
     
