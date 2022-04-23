9e166b0a285884c1d2ac15be1aeb349b5d9979db
Vivek Pathak <87029058+vp2312@users.noreply.github.com>
2022-04-23 10:56:30 +0530

    Time: 32 ms (44.97%), Space: 11.2 MB (55.36%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,46 +1,35 @@
 class Solution {
+    public:
+    int t[1050][1050];
+    Solution()
+    {
+        memset(t,-1,sizeof(t));
+    }
+    int sub(string& S1, string& S2, int n, int m)
+    {
+        if(n==0 || m==0)
+        {
+            return 0;
+        }
+        if(t[n][m]!=-1)
+        {
+            return t[n][m];
+        }
+        if(S1[n-1]==S2[m-1])
+        {
+            return t[n][m]= 1+sub(S1, S2, n-1, m-1);
+        }
+        else
+        {
+            return t[n][m]= max(sub(S1, S2, n,m-1), sub(S1, S2, n-1,m));
+        }
+        return t[n][m];
+    };
+    
 public:
-    int longestCommonSubsequence(string a, string b) {
-        int n=a.length();
-        int m= b.length();
-        int dp[n+1][m+1];
-        
-        for(int i=0;i<n+1;i++)
-        {
-            for(int j=0;j<m+1;j++)
-            {
-                if(i==0||j==0)
-                {
-                    dp[i][j]=0;
-                }
-            }
-        }
-        for(int i=1;i<n+1;i++)
-        {
-            for(int j=1;j<m+1;j++)
-            {
-                if(a[i-1]==b[j-1])
-                {
-                    dp[i][j]=1+dp[i-1][j-1];
-                }
-                else
-                {
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-                }
-            }
-        }
-        
-         for(int i=0;i<n+1;i++)
-        {
-            for(int j=0;j<m+1;j++)
-            {
-                    cout<<dp[i][j]<<" ";
-                
-            }
-            cout<<endl;
-        }
-        
-        return dp[n][m];
-        
+    int longestCommonSubsequence(string text1, string text2) {
+        int len1=text1.length();
+        int len2=text2.length();
+        return sub(text1,text2,len1,len2);
     }
 };
