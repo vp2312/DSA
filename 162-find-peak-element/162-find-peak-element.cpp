f83623b6b0f94408cdfa1f94b148969f30b94411
Vivek Pathak <87029058+vp2312@users.noreply.github.com>
2022-04-27 22:47:37 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (5.26%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -15,18 +15,19 @@ public:
                 {
                     return mid;
                 }
-                else if(arr[mid]> arr[mid-1])
+                else if(arr[mid]< arr[mid-1])
+                {
+                    hi=mid-1;
+                    
+                }
+                else if(arr[mid]< arr[mid+1])
                 {
                     lo= mid+1;
                 }
-                else if(arr[mid]> arr[mid+1])
-                {
-                    hi=mid-1;
-                }
-                else if(arr[mid]< arr[mid+1] || arr[mid]< arr[mid-1])
-                {
-                    hi=mid-1;
-                }
+                // else if(arr[mid]< arr[mid+1] || arr[mid]< arr[mid-1])
+                // {
+                //     hi=mid-1;
+                // }
                 else ;
             }
             else if( mid==0)
