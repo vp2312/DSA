27220a88df3906b821477544714a001ec9ad8db6
Vivek Pathak <87029058+vp2312@users.noreply.github.com>
2022-05-04 17:32:23 +0530

    Time: 247 ms (26.49%), Space: 67.6 MB (43.68%) - LeetHub

---
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        unordered_map<int,int> mp;
+        for(auto j:nums)
+        {
+            mp[j]++;
+        }
+        // for(auto j:mp)
+        // {
+        //     cout<<j.first<<" "<<j.second<<endl;
+        // }
+        // cout<<endl;
+        int cnt=0;
+        for(auto j :nums)
+        {
+            int sec=k-j;
+            mp[j]--;
+            if(mp.find(sec)!=mp.end() )
+            {
+                if(mp[sec]>0)
+                {
+                    
+                    cnt++;
+                    mp[sec]--;
+                    
+                    // cout<<j<<" "<<sec<<" "<<endl;
+                    
+                }
+                // else
+                // {
+                //      mp[j]++;
+                // }    
+            }
+            // else
+            // {
+            //      mp[j]++;
+            // }
+        }
+        // cout<<endl;
+        return cnt;
+    }
+};
