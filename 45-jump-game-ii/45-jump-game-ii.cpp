e92ba73582a9953beb1262a82e9f912d20353363
Vivek Pathak <87029058+vp2312@users.noreply.github.com>
2022-05-10 22:16:57 +0530

    Time: 18 ms (77.63%), Space: 16.4 MB (96.84%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -4,7 +4,7 @@ public:
         
         int mxre=0;
         int currmx=0;
-        int jmp=0;
+        int jmp=0;                          //unreachable wala case eliminate kara h ques me
         for(int i=0; i<nums.size()-1;i++)
         {
             // currmx=nums[i];
