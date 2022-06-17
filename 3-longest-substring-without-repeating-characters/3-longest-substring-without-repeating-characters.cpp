f983175a6fb69524509f74fcbe874519d18a0bb4
Vivek Pathak <87029058+vp2312@users.noreply.github.com>
2022-06-17 12:06:14 +0530

    Time: 19 ms (65.53%), Space: 8.2 MB (74.15%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -9,7 +9,6 @@ public:
         int ans=1;
         while(j<s.size())
         {
-            cout<<j<<" ";
             mp[s[j]]++;
             if(mp[s[j]]==1)     cnt++;
             
@@ -18,7 +17,6 @@ public:
                 ans=max(ans,j-i+1);
                 j++;
             }
-            // if(j == s.size())      ans=max(ans,j-i);
             else if(cnt< (j-i+1) )
             {
                 while(s[i]!=s[j])
@@ -31,34 +29,10 @@ public:
                 if(mp[s[i]]==0)     cnt--;
                 i++;
                 j++;
-                // cout<<ans<<"...";
-                // j++;
             }
-//             else if(cnt>k)
-//             {
-                
-//             }
             else    ;
         }
         if(ans<0)   return 0;
         return ans;
-//         unordered_set <char> sub_strng;
-        
-//         int i = 0, j = 0, n = s.size(), ans = 0;
-//         while(j < n)
-//         {
-//             if(sub_strng.find(s[j]) == sub_strng.end())
-//             {
-//                 sub_strng.insert(s[j]);
-//                 j++;
-//                 ans = max(ans, j - i);
-//             }
-//             else
-//             {
-//                 sub_strng.erase(s[i]);
-//                 i++;
-//             }
-//         }
-//         return ans;
     }
 };
