88567fdf2c312d68bcfc2199c0c5ca8caa756a6f
Vivek Pathak <87029058+vp2312@users.noreply.github.com>
2022-06-17 12:03:56 +0530

    Time: 75 ms (13.83%), Space: 8.4 MB (46.72%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,23 +1,64 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_set <char> sub_strng;
-        
-        int i = 0, j = 0, n = s.size(), ans = 0;
-        while(j < n)
+        if(s.empty())   return 0;
+        int i=0;
+        int j=0;
+        unordered_map<char,int> mp;
+        int cnt=0;
+        int ans=1;
+        while(j<s.size())
         {
-            if(sub_strng.find(s[j]) == sub_strng.end())
+            cout<<j<<" ";
+            mp[s[j]]++;
+            if(mp[s[j]]==1)     cnt++;
+            
+            if(cnt==(j-i+1)) 
             {
-                sub_strng.insert(s[j]);
+                ans=max(ans,j-i+1);
                 j++;
-                ans = max(ans, j - i);
             }
-            else
+            // if(j == s.size())      ans=max(ans,j-i);
+            else if(cnt< (j-i+1) )
             {
-                sub_strng.erase(s[i]);
+                while(s[i]!=s[j])
+                {
+                        mp[s[i]]--;
+                        if(mp[s[i]]==0)     cnt--;
+                        i++;
+                }
+                mp[s[i]]--;
+                if(mp[s[i]]==0)     cnt--;
                 i++;
+                j++;
+                // cout<<ans<<"...";
+                // j++;
             }
+//             else if(cnt>k)
+//             {
+                
+//             }
+            else    ;
         }
+        if(ans<0)   return 0;
         return ans;
+//         unordered_set <char> sub_strng;
+        
+//         int i = 0, j = 0, n = s.size(), ans = 0;
+//         while(j < n)
+//         {
+//             if(sub_strng.find(s[j]) == sub_strng.end())
+//             {
+//                 sub_strng.insert(s[j]);
+//                 j++;
+//                 ans = max(ans, j - i);
+//             }
+//             else
+//             {
+//                 sub_strng.erase(s[i]);
+//                 i++;
+//             }
+//         }
+//         return ans;
     }
 };
