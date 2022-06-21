30e7c64638dd58d95b0eee63394cece9049fe388
Vivek Pathak <87029058+vp2312@users.noreply.github.com>
2022-06-21 11:30:21 +0530

    Time: 7 ms (68.03%), Space: 16.2 MB (93.68%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -11,41 +11,22 @@
  */
 class Solution {
 public:
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        if((p==NULL && q!=NULL) || (p!=NULL && q==NULL))  return false;
+    bool isSameTree(TreeNode* p, TreeNode* q)
+    {
+        
         if((p==NULL && q==NULL))    return true;
-//          if((p->val==q->val))
-//          {
-//              bool l=isSameTree(p->left,q->left);
-//             bool r=isSameTree(p->right,q->right);
-//             if(l && r)  return true;
-             
-//          }
+        if((p==NULL || q==NULL))    return false;
+        
         bool l=isSameTree(p->left,q->right);
         bool r=isSameTree(p->right,q->left);
         if((p->val==q->val) && l && r)  return true;
         return false;
         
     }
+    
     bool isSymmetric(TreeNode* root) {
         
         return isSameTree(root->left,root->right);
-        
-//          if((root->left==NULL && root->right!=NULL) || (root->left!=NULL && root->right==NULL))  return false;
-//         if((root->left==NULL && root->right==NULL))    return true;
-//         if((root->left->val==root->right->val)) return true;
-//         else return false;
-// //          if((p->val==q->val))
-// //          {
-// //              bool l=isSameTree(p->left,q->left);
-// //             bool r=isSameTree(p->right,q->right);
-// //             if(l && r)  return true;
-             
-// //          }
-//         bool l=isSymmetric(root->left);
-//         bool r=isSymmetric(root->right);
-//         if(/*(root->left->val==root->right->val) &&*/ l && r)  return true;
-//         return false;
         
     }
 };
