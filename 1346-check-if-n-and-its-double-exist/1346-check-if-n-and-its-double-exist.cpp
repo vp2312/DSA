f1e47ee0c0b2aee3ce2e61b1d015234e87e387eb
Vivek Pathak <87029058+vp2312@users.noreply.github.com>
2022-07-13 00:08:07 +0530

    Time: 8 ms (75.93%), Space: 10 MB (57.58%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -9,30 +9,6 @@ public:
                 flag=1;
                 continue;
             }
-//             if(j<0)
-//             {
-//                 j=j*-1;
-//                 if(j%2==0)
-//             {
-                    
-//                 if(find(arr.begin(),arr.end(),-j)!=arr.end() && (find(arr.begin(),arr.end(),-j/2)!=arr.end() || find(arr.begin(),arr.end(),-j*2)!=arr.end()))
-//                 {
-//                     // cout<<*j;
-//                     return true;
-//                 }
-//             }
-//             else
-//             {
-//                 if(find(arr.begin(),arr.end(),-j)!=arr.end() && (find(arr.begin(),arr.end(),-j*2)!=arr.end()))
-//                 {
-//                     // /cout<<*j;
-//                     return true;
-//                 }
-                
-//             }
-//             }
-//             else
-//             {
             if(j%2==0)
             {
                 if(find(arr.begin(),arr.end(),j)!=arr.end() && (find(arr.begin(),arr.end(),j/2)!=arr.end() || find(arr.begin(),arr.end(),j*2)!=arr.end()))
