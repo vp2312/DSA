246344a1a7246bc305e2522eeb4700715ab0f7a7
Vivek Pathak <87029058+vp2312@users.noreply.github.com>
2022-07-29 18:42:19 +0530

    Time: 12 ms (43.78%), Space: 8.9 MB (57.54%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -2,78 +2,61 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& arr, vector<int>& nums2) {
         unordered_map<int, int> mp;
-//         for(auto j: arr)
-//         {
-//             mp.insert({j, -9});
-//         }
-//         for(auto j: mp)
-//         {
-//             cout<<j.first<<" ";
-//         }
+        for(auto j: arr)
+        {
+            mp.insert({j, -9});
+        }
         
         int n=nums2.size();
-        vector<int>v ;
+        vector<int>v , v1;
         stack<int>s;
         
         for(int i= n-1;i>=0 ;i--)
         {
-//             if(s.size() == 0) 
-//             {
-//                 if(mp.find(nums2[i])!=mp.end())
-//                 {
-//                     mp[nums2[i]]=-1;
-//                 }
-//             }
-//             else if(s.size() > 0 && s.top() > nums2[i] )
-//             {
-//                     if(mp.find(nums2[i])!=mp.end())
-//                     {
-//                         mp[nums2[i]]=s.top();
-//                     }
-//             }
-//             else if(s.size()>0 && s.top() <= nums2[i])
-//             {
-//                 while(s.size()>0 && s.top() <= nums2[i])    s.pop();
+            if(s.size() == 0) 
+            {
+                if(mp.find(nums2[i])!=mp.end())
+                {
+                    mp[nums2[i]]=-1;
+                }
+            }
+            else if(s.size() > 0 && s.top() > nums2[i] )
+            {
+                    if(mp.find(nums2[i])!=mp.end())
+                    {
+                        mp[nums2[i]]=s.top();
+                    }
+            }
+            else if(s.size()>0 && s.top() <= nums2[i])
+            {
+                while(s.size()>0 && s.top() <= nums2[i])    s.pop();
                 
-//                 if(s.size() == 0)
-//                 {
-//                     if(mp.find(nums2[i])!=mp.end())
-//                     {
-//                         mp[nums2[i]]=-1;
-//                     }
-//                 }
-//                 else
-//                 {
-//                     if(mp.find(nums2[i])!=mp.end())
-//                     {
-//                         mp[nums2[i]]=s.top();
-//                     }
-//                 }
-//             }
-            while(!s.empty()&&s.top()<=nums2[i]){
-                s.pop();
-            }
-            if(s.empty()){
-                mp[nums2[i]]=-1;
-            }
-            else{
-                mp[nums2[i]]=s.top();
+                if(s.size() == 0)
+                {
+                    if(mp.find(nums2[i])!=mp.end())
+                    {
+                        mp[nums2[i]]=-1;
+                    }
+                }
+                else
+                {
+                    if(mp.find(nums2[i])!=mp.end())
+                    {
+                        mp[nums2[i]]=s.top();
+                    }
+                }
             }
             s.push(nums2[i]);
         }
-        
-        for(auto n:arr){
-            v.push_back(mp[n]);
+        for(auto j: arr)
+        {
+            // cout<<j.first<<" ";
+            v.push_back(mp[j]);
+            //v1.push_back(j.first);
         }
-//         for(auto j: mp)
-//         {
-//             // cout<<j.first<<" ";
-//             v.push_back(j.second);
-//             v1.push_back(j.first);
-//         }
         
-//         reverse(v.begin(),v.end());
-//         reverse(v1.begin(),v1.end());
+        //reverse(v.begin(),v.end());
+        //reverse(v1.begin(),v1.end());
         // for(auto j: v1)
         // {
         //     cout<<j<<" ";
